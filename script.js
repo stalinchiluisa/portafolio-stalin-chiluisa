1fb6c56ceee58ca18153973189a8f836aa8504d4
stalinchiluisa <s.chiluisa@ecotec.edu.ec>
2026-08-25 18:02:48 -0500

    Implementado modo oscuro con LocalStorage

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("JavaScript cargado correctamente");
+
+const botonTema = document.getElementById("boton-tema");
+
+function cargarTemaGuardado() {
+    const temaGuardado = localStorage.getItem("tema");
+
+    if (temaGuardado === "oscuro") {
+        document.body.classList.add("modo-oscuro");
+        botonTema.textContent = "☀️ Modo claro";
+    }
+}
+
+function cambiarTema() {
+    document.body.classList.toggle("modo-oscuro");
+
+    if (document.body.classList.contains("modo-oscuro")) {
+        localStorage.setItem("tema", "oscuro");
+        botonTema.textContent = "☀️ Modo claro";
+    } else {
+        localStorage.setItem("tema", "claro");
+        botonTema.textContent = "🌙 Modo oscuro";
+    }
+}
+
+botonTema.addEventListener("click", cambiarTema);
+
+cargarTemaGuardado();
